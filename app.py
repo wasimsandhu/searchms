e5f0e772861a2fdda7f5a14dc2b7dc0d5e6441dc
Wasim Sandhu <sandhuwasim@gmail.com>
2023-07-13 19:31:57 -0700

    UI for user input panel

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-import sys, webbrowser
+import sys, webbrowser, ast
+
 from dash import Dash, html, dcc, callback, Output, Input
 from dash.exceptions import PreventUpdate
 import dash_bootstrap_components as dbc
-import plotly.express as px
 import pandas as pd
+import numpy as np
+import matchms
+from plots import *
 
 local_stylesheet = {
     "href": "https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap",
@@ -18,15 +21,108 @@ def serve_layout():
 
     return html.Div([
         
+        # Navigation bar
+        dbc.Navbar(color="dark", dark=True, children=[
+            dbc.Container(style={"height": "50px"}, children=[
+                html.A(
+                    dbc.Row([
+                        dbc.Col(align="center", className="g-0", children=[
+                            dbc.NavbarBrand(id="header", children="SearchMS", className="ms-2")
+                        ])
+                    ]), href="https://localhost:8050", style={"textDecoration": "none"}
+                )
+            ])
+        ]),
+        
+        # App layout
+        html.Div(className="app-layout", children=[
+            dbc.Row(justify="center", children=[
+                
+                # Panel for search input
+                dbc.Col(width=4, children=[
+                    
+                    # Input group for loading mass spectrum
+                    html.Div([
+                        dbc.Label("Mass spectrum (.msp, .json, .mgf)"),
+                        dbc.InputGroup([
+                            dbc.Textarea(rows=5, id="mass-spectrum-input", placeholder="Enter your mass spectrum here"),
+                            dbc.FormFeedback("Looks good!", type="valid"),
+                            dbc.FormFeedback("Please ensure the peak table of your mass spectrum is in one of the accepted formats.", type="invalid"),
+                        ]),
+                    ]), html.Br(),
+                    
+                    # Input group for loading reference library
+                    html.Div([
+                        dbc.Label("Load mass spectral reference library (.msp, .json, .mgf)"),
+                        dbc.InputGroup([
+                            dbc.Input(id="reference-library", placeholder="No file selected"),
+                            dbc.Button(dcc.Upload(
+                                id="load-reference-library-button",
+                                accept="text/plain, .msp",
+                                children=[html.A("Browse Files")]),
+                                color="secondary"),
+                            dbc.FormFeedback("Looks good!", type="valid"),
+                            dbc.FormFeedback("Please ensure your spectral library is in one of the accepted formats.", type="invalid"),
+                        ]),
+                    ]), html.Br(),
+                    
+                    html.H6("or", style={"text-align": "center"}),
+                    
+                    # Input group for choosing a mass spectrometry database
+                    html.Div([
+                        dbc.Label("Search against public databases"),
+                        dbc.Select(id="reference-database", placeholder="Choose public database...", options=[
+                            {"label": "", "value": ""},
+                            {"label": "GNPS", "value": "GNPS"},
+                            {"label": "MONA", "value": "MONA"},
+                            {"label": "MassBank", "value": "MassBank"},
+                            {"label": "Berkeley Lab", "value": "Berkeley Lab"},
+                            {"label": "CASMI", "value": "CASMI"}
+                        ]),
+                        dbc.FormFeedback("Looks good!", type="valid")
+                    ]), html.Br(),
+                    
+                    # Plot for user-inputted mass spectrum
+                    html.Div(className="plot-container", children=[
+                        dcc.Graph(id="experimental-spectrum"),
+                    ])
+                ]),
+                
+                # Panel for search results
+                dbc.Col(width=8, children=[
+                    
+                ])
+            ])
+        ])
     ])
+
+app.layout = serve_layout
+
+@app.callback(Output("experimental-spectrum", "figure"),
+              Input("mass-spectrum-input", "value"), prevent_initial_call=True)
+def render_user_inputted_spectrum(peak_data):
+    
+    """
+    Renders plot for user-inputted mass spectrum.
+    """
+    
+    # Convert string into object
+    mass_spectrum = ast.literal_eval(peak_data)
+    
+    # Initialize DataFrame
+    df_peaks = pd.DataFrame(np.array(mass_spectrum), columns=["m/z", "intensity"])
+    
+    # Render plot
+    return render_mass_spectrum(df_peaks)
+
 
 if __name__ == "__main__":
     
-    if sys.platform == "win32":
-        chrome_path = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-        webbrowser.register("chrome", None, webbrowser.BackgroundBrowser(chrome_path))
-        webbrowser.get("chrome").open("http://127.0.0.1:8050/")
-    elif sys.platform == "darwin":
-        webbrowser.get("chrome").open("http://127.0.0.1:8050/", new=1)
+    # if sys.platform == "win32":
+    #     chrome_path = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+    #     webbrowser.register("chrome", None, webbrowser.BackgroundBrowser(chrome_path))
+    #     webbrowser.get("chrome").open("http://localhost:8050/")
+    # elif sys.platform == "darwin":
+    #     webbrowser.get("chrome").open("http://localhost:8050/", new=1)
         
     app.run(debug=True)
---
+++ plots.py
@@ -0,0 +1,60 @@
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
+def render_mass_spectrum(df_peaks):
+    
+    """
+    Renders plot of mass spectrum.
+    
+    Args:
+        df_peaks (pd.DataFrame): User-provided peak table
+    
+    Returns:
+        plotly.Figure: Mass spectrum
+    """
+    
+    min_mz = df_peaks["m/z"].min() - 10
+    max_mz = df_peaks["m/z"].max() + 10
+    
+    plot = px.bar(df_peaks, title="Experimental Spectrum", x="m/z", y="intensity", height=600)
+    plot.update_layout(showlegend=False, transition_duration=500, clickmode="event", margin=dict(t=75, b=75, l=0, r=0))
+    plot.update_xaxes(title="", range=[min_mz, max_mz], side="bottom")
+    plot.update_yaxes(title="")
+    plot.update_traces(textposition="outside", hovertemplate="m/z: %{x}<br>Intensity: %{y}<br>")
+    
+    return plot
+    
+
+def render_head_to_tail(df_head, df_tail):
+
+    """
+    Renders head-to-tail plot for spectral comparison
+    
+    Args:
+        df_head (DataFrame): Experimental spectrum
+        df_tail (DataFrame): Reference spectrum from library / database
+    
+    Returns:
+        plotly.Figure: Head-to-tail spectral comparison plot
+    """
+
+    min_mz = min(df_head["m/z"].min(), df_tail["m/z"].min()) - 10
+    max_mz = max(df_head["m/z"].max(), df_tail["m/z"].max()) + 10
+
+    plot = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0)
+
+    plot.append_trace(go.Bar(x=df_head["m/z"], y=df_head["Intensity"], marker=dict(color="rgb(0, 123, 255)")), row=1, col=1)
+    plot.append_trace(go.Bar(x=df_tail["m/z"], y=df_tail["Intensity"], marker=dict(color="rgb(220, 53, 69)")), row=2, col=1)
+
+    plot.update_xaxes(range=[min_mz, max_mz])
+    plot.update_xaxes(showticklabels=True, row=1, col=1)
+    
+    plot.update_xaxes(title="Experimental", row=1, col=1)
+    plot.update_xaxes(title="Reference", row=2, col=1)
+    plot.update_yaxes(autorange="reversed", row=2, col=1)
+
+    plot.update_traces(width=1, hovertemplate="m/z: %{x}<br>Intensity: %{y}<br>")
+    plot.update_layout(height=height, showlegend=False, margin=dict(t=0, b=0, l=0, r=0), xaxis={'side': 'top'})
+
+    return plot
